94d04e85022d1b140bddbfc949e95be32b5795fc
Jensen Li <jensenli049@gmail.com>
2020-06-29 18:55:16 -0400

    Fixed current freebie bug. Seems to be working. Need more testing

--- aiturn.cpp
+++ aiturn.cpp
@@ -24,8 +24,9 @@ int Evaluate_Board(char freebie[9], char current[9]){
 }
 
 int MiniMax(char main[9][9], char freebie[9], char current[9], bool ai, int score, int depth){
+    //printf("Depth: %d\tScore: %d\tAI: %d\n", depth, score, ai);
     // Limit search depth
-    if(depth > 2)
+    if(depth > 4)
         return score;
     
     // Make a copy of current board state
@@ -33,13 +34,14 @@ int MiniMax(char main[9][9], char freebie[9], char current[9], bool ai, int scor
     char temp_freebie[9];
     copy(&main[0][0], &main[9][9], &temp_main[0][0]);
     copy(&freebie[0], &freebie[9], &temp_freebie[0]);
-
+    
     // In the case of a freebie, evaluate every freebie square
+    //cout << "Check_Win(current) = " << Check_Win(current) << endl;
     if(Check_Win(current) != ' '){
         for(int i = 0; i < 9; i++){
             if(freebie[i] == ' ' && ai)
                 MiniMax(main, freebie, main[i], ai, score-50, depth);
-            else
+            else if(freebie[i] == ' ')
                 MiniMax(main, freebie, main[i], ai, score+50, depth);
         }
     }
@@ -50,7 +52,9 @@ int MiniMax(char main[9][9], char freebie[9], char current[9], bool ai, int scor
             if(current[i] == ' '){
                 current[i] = 'o'; // Make a move
                 Set_Board_Winner(main, freebie); // Update board
+                //Print_Main_Board(main);
                 int value = Evaluate_Board(freebie, current);
+                //printf("AI Value: %d\n", value);
                 if(value == -1000) return value; // If game is won
                 best = min(best, MiniMax(main, freebie, main[i], !ai, score+value, depth+1));
                 // Undo the move and its effects
@@ -66,7 +70,9 @@ int MiniMax(char main[9][9], char freebie[9], char current[9], bool ai, int scor
             if(current[i] == ' '){
                 current[i] = 'x'; // Make a move
                 Set_Board_Winner(main, freebie); // Update board
+                //Print_Main_Board(main);
                 int value = Evaluate_Board(freebie, current);
+                //printf("Player Value: %d\n", value);
                 if(value == 1000) return value; // If game is won
                 best = max(best, MiniMax(main, freebie, main[i], !ai, score+value, depth+1));
                 // Undo the move and its effects
@@ -94,16 +100,17 @@ int AI_Turn(char main[9][9], char freebie[9], char current[9]){
 
     for(int i = 0; i < 9; i++){ // Checking all squares
         int mvalue = best_value+1;
+        
         // Case of freebie
         if(current == freebie)
             mvalue = MiniMax(main, freebie, main[i], true, 0, 0);
-        
+
         // Normal Turn
         else if(current[i] == ' '){
             current[i] = 'o'; // Make a move
             Set_Board_Winner(main, freebie); // Update board
             if(Evaluate_Board(freebie, current) == -1000) return -1000; // If game is won
-            mvalue = MiniMax(main, freebie, main[i], false, 0, 0);
+            mvalue = MiniMax(main, freebie, main[i], true, 0, 0);
             // Undo the move and its effects
             copy(&temp_main[0][0], &temp_main[9][9], &main[0][0]);
             copy(&temp_freebie[0], &temp_freebie[9], &freebie[0]);
